58a5249a60ec60c82e221ebae127dd596de6ee5e
Arin Nur Hakim_146 <134836192+arinnurhakim@users.noreply.github.com>
2024-04-06 16:14:48 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+// Close the menu when a menu item is clicked
+menu.addEventListener("click", (e) => {
+  if (e.target.tagName === "A") {
+    menu.classList.remove("open");
+  }
+});
